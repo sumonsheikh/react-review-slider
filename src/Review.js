6f5082b04d4146cb9a480f84eb0899bc13b2ee10
sumonsheikh <sm.sumon117@gmail.com>
2020-12-08 12:09:47 +0600

    create and add functionality to the review component

--- src/Review.js
+++ src/Review.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React , {useState} from 'react';
+import people from './data';
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 
  const Review = () => {
+     const [index, setIndex]        = useState(0);
+     const {name, image, job, text} = people[index];
+     //preview Btn handler
+     const prevReview = (index) =>{
+        setIndex( (index) =>{
+            let newIndex = index - 1;
+            return newIndex;
+        })
+     }
+
+     //next Btn handler
+     const nextReview = () =>{
+         setIndex( (index) =>{
+             let newIndex = index + 1;
+             return newIndex;
+         })
+     }
+     const randomReview = () =>{
+         
+     }
     return (
-        <div>
-            <h1>Review component</h1>
-        </div>
-    )
+        <article className="review">
+            <div className="image-container">
+                <img src={image} alt={name} className="person-image"/>
+                <span className="quote-icon">
+                    <FaQuoteRight/>
+                </span>
+            </div>
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+            <div className="button-container">
+                <button className="prev-btn" onClick={prevReview}>
+                    <FaChevronLeft/>
+                </button>
+                <button className="next-btn" onClick={nextReview}>
+                    <FaChevronRight/>
+                </button>
+            </div>
+            <button className="random-btn" onClick={randomReview}>Surprise me</button>
+        </article>
+    );
 }
 export default Review;
