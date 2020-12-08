d2f855c355a7110dbba3c289fcac24ecf0975ed8
sumonsheikh <sm.sumon117@gmail.com>
2020-12-08 14:09:12 +0600

    fix the next and preview button

--- src/Review.js
+++ src/Review.js
@@ -5,11 +5,23 @@ import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
  const Review = () => {
      const [index, setIndex]        = useState(0);
      const {name, image, job, text} = people[index];
+
+     //check index where it's up the length or belew the length
+     const checkNumber = (number) =>{
+        if(number > people.length-1){
+            return 0;
+        }
+        if(number < 0){
+            return people.length-1;
+        }
+        return number;
+     }
+
      //preview Btn handler
      const prevReview = (index) =>{
         setIndex( (index) =>{
             let newIndex = index - 1;
-            return newIndex;
+            return checkNumber(newIndex);
         })
      }
 
@@ -17,11 +29,12 @@ import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
      const nextReview = () =>{
          setIndex( (index) =>{
              let newIndex = index + 1;
-             return newIndex;
+             return checkNumber(newIndex);
          })
      }
+     //random review 
      const randomReview = () =>{
-         
+
      }
     return (
         <article className="review">
